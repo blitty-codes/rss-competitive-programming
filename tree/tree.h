6aad549bc7109a5a5492bf13ab1c785334b76568
blitty-codes <blittynet@gmail.com>
2021-04-29 18:50:15 +0200

    no space

--- tree/tree.h
+++ tree/tree.h
@@ -12,18 +12,18 @@
 		Search for the node that has a given value
 		return a nullptr or the reference to the node
 	*/
-	TreeNode *search (Tree *tree, void* value);
+	TreeNode *search(Tree *tree, void* value);
 
 	/*
 		This function adds a node and if needed, it reordenates the tree
 		return a nullptr otherwise a reference to the node added
 	*/
-	TreeNode *add_node (Tree *tree, TreeNode *node);
+	TreeNode *add_node(Tree *tree, TreeNode *node);
 
 	/*
 		NULL - correctly deleted
 		reference to node - otherwise
 	*/
-	TreeNode *remove_node (Tree *tree, TreeNode *node);
+	TreeNode *remove_node(Tree *tree, TreeNode *node);
 
 #endif
