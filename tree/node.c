7142738a6aa13d91386b31c3da53bc6b08a1d5d4
blitty-codes <blittynet@gmail.com>
2021-04-29 18:51:42 +0200

    functions from libreary node.h defined

---
+++ tree/node.c
@@ -0,0 +1,44 @@
+#include "node.h"
+
+TreeNode *create_node(void *value, TreeNode *rNode, TreeNode *lNode)
+{
+	/*
+		Another way of init a struct (not dymaic)
+		TreeNode p = {
+			value: "Hello World!",
+			rNode: NULL,
+			lNode: NULL,
+		};
+	*/
+	TreeNode *new_node = (TreeNode *) calloc(1, sizeof(TreeNode));
+	new_node->value = value;
+	new_node->lNode = lNode;
+	new_node->rNode = rNode;
+
+	return new_node;
+}
+
+void delete_node(TreeNode *node)
+{
+	free(node);
+}
+
+TreeNode* right_node(TreeNode *node)
+{
+	return node->rNode;
+}
+
+TreeNode* left_node(TreeNode *node)
+{
+	return node->lNode;
+}
+
+void set_value(TreeNode *node, void *value)
+{
+	node->value = value;
+}
+
+short int is_leaf(TreeNode *node)
+{
+	return (node->rNode == NULL && node->lNode == NULL) ? TRUE : FALSE;
+}
